f7e5ce1ef7ae7ad85a1354118b4c6002eeb29ac1
Kevin <nowakk96@icloud.com>
2022-02-17 10:06:27 +0100

    Updated

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman  {
@@ -7,8 +10,28 @@ public class Hangman  {
 
         Scanner scanner = new Scanner(new File("/home/spaceranger96/Downloads/usa.txt"));
 
+        List<String> words = new ArrayList<>();
+
         while(scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+            words.add(scanner.nextLine());
         }
+
+        Random rand = new Random();
+
+        String word = words.get(rand.nextInt(words.size()));
+
+        System.out.println(word);
+
+        List<Character> playerGuesses = new ArrayList<>();
+
+        for(int i = 0; i < word.length(); i++) {
+            if(playerGuesses.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+            }
+            else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
     }
 }
