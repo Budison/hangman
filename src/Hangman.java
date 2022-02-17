8b2248d093d293c0b178103a19661a966cb7462d
Kevin <nowakk96@icloud.com>
2022-02-17 11:11:07 +0100

    Updated

--- src/Hangman.java
+++ src/Hangman.java
@@ -8,30 +8,58 @@ import java.util.Scanner;
 public class Hangman  {
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner scanner = new Scanner(new File("/home/spaceranger96/Downloads/usa.txt"));
-
+        // "user.dir" gives us the program's working directory
+        Scanner scanner = new Scanner(new File(System.getProperty("user.dir") + "/assets/usa.txt"));
+        Scanner keyboard = new Scanner(System.in);
+        
+        // This list will contain all the words from the file (easier to work with)
         List<String> words = new ArrayList<>();
 
+        // Loop to add all the words into our words list
         while(scanner.hasNext()) {
             words.add(scanner.nextLine());
         }
 
+        // Randomizer to pick a random word from words (for Single Player Mode)
         Random rand = new Random();
 
+        // words.size() to get the upper bound for rand.nextInt()
         String word = words.get(rand.nextInt(words.size()));
 
         System.out.println(word);
 
+        // List of Characters the player has guessed
         List<Character> playerGuesses = new ArrayList<>();
 
+        while(true) {
+            getPlayerGuess(keyboard, word, playerGuesses);
+            if(printWordState(word, playerGuesses)) break;
+        }
+
+        System.out.println("You win!");
+
+
+    }
+
+    private static void getPlayerGuess(Scanner keyboard, String word, List<Character> playerGuesses) {
+        System.out.print("Please enter a letter: ");
+        String letterGuess = keyboard.nextLine();
+        playerGuesses.add(letterGuess.charAt(0));
+    }
+
+    private static boolean printWordState(String word, List<Character> playerGuesses) {
+        // Check what char's the player has guessed
+        int correctCount = 0;
         for(int i = 0; i < word.length(); i++) {
             if(playerGuesses.contains(word.charAt(i))) {
                 System.out.print(word.charAt(i));
-            }
-            else {
+                correctCount++;
+            } else {
                 System.out.print("-");
             }
         }
         System.out.println();
+
+        return word.length() == correctCount;
     }
 }
